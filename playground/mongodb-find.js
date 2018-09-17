feaf7b1aaedface58597b89a5bba83066d77a04f
Stuart Doney <stu@stuarts-air.home>
2018-09-17 10:02:35 +0100

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({_id: new ObjectID('5b9f675e79bf39173784e830')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+
+    db.collection('Users').find({name: 'Stuart'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fect Todos', err)
+    })
+
+    //db.close();
+})
